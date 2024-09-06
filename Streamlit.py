5c2363854c2ae33d221fc6ad93ce3f1d9c59b306
SyahidHalid <78252704+SyahidHalid@users.noreply.github.com>
2024-09-06 09:12:18 +0800

    Update Streamlit - Upload file.py

--- Streamlit.py
+++ Streamlit.py
@@ -2,3 +2,14 @@ import streamlit as st
 
 st.write('Hello World!')
 
+st.set_page_confiq(page_title = 'File Uploader')
+
+df = st.file_uploader(label= "Upload your dataset:")
+
+if df:
+  df = pd.read_csv(df)
+
+st.write(df.head(5))
+  
+
+
