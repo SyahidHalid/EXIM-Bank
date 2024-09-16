5734a69812051fb1096d97edf5a27b9a6fe271b3
SyahidHalid <syahidhalid@gmail.com>
2024-09-17 00:53:36 +0800

    Update 20240917

--- Streamlit_Income_Statement.py
+++ Streamlit_Income_Statement.py
@@ -10,32 +10,37 @@ pd.set_option("display.max_colwidth", 1000) #huruf dlm column
 pd.set_option("display.max_rows", 100)
 pd.set_option("display.precision", 2) #2 titik perpuluhan
 
-st.set_page_config(page_title = 'File Uploader')
 
-st.write('## Title')
-st.write('Testing for income statement')
-X = st.text_input("Fovarite Movie?")
+#st.set_page_config(page_title='Title Dashboard') #, page_icon=r'EXIM-Bank//BP_EXIM_SME.jpg' ,layout='wide'
+#st.set_page_config(page_title = 'File Uploader')
 
-is_clicked = st.button("Click Me")
+#----------------------------Title--------------------------------------------------------------------
 
-chart_data = pd.DataFrame(np.random.randn(20,3),columns=["a","b","c"])
+st.write('## Income Statement')
+st.write('To upload trial balance received')
 
-st.bar_chart(chart_data)
-st.line_chart(chart_data)
+#----------------------------Input--------------------------------------------------------------------
 
-df1 = st.file_uploader(label= "Upload your dataset 1:")
+X = st.text_input("Input Date (i.e. 202409):")
+Y = st.text_input("Input Name (i.e. 09. Income statement Sep 2024):")
+
+#----------------------------Upload--------------------------------------------------------------------
+
+df1 = st.file_uploader(label= "Upload EXIB:")
+df2 = st.file_uploader(label= "Upload EXIM:")
+df3 = st.file_uploader(label= "Upload EXTF:")
+
 if df1:
   df1 = pd.read_excel(df1, header=5)
   st.write(f"Your favorite movie is:{X}")
   st.write(df1.head())
-
   
-df2 = st.file_uploader(label= "Upload your dataset 2:")
+
 if df2:
   df2 = pd.read_excel(df2, header=5)
   st.write(df2.head())
 
-df3 = st.file_uploader(label= "Upload your dataset 3:")
+
 if df3:
   df3 = pd.read_excel(df3, header=5)
   df3.rename(columns={"C":"Unnamed:_1",
@@ -50,6 +55,19 @@ if df3:
                             "Unnamed:_10":"Unnamed:_10"}, inplace=True)
   st.write(df3.head())
   #st.write()
+
+#----------------------------Run--------------------------------------------------------------------
+
+is_clicked = st.button("Run")
+
+#----------------------------Testing Graph--------------------------------------------------------------
+
+chart_data = pd.DataFrame(np.random.randn(20,3),columns=["a","b","c"])
+
+st.bar_chart(chart_data)
+st.line_chart(chart_data)
+
+#----------------------------Export--------------------------------------------------------------------
 
 
 st.link_button("Youtube",url="https://www.youtube.com/watch?v=D0D4Pa22iG0")
